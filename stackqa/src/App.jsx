c718394174241a93454b0bb2ad547ddbf4530fc1
mik284 <mikeykamau222@gmail.com>
2022-11-30 18:03:41 +0300

    Created stackqa frontend with login and register page and tailwind configs

---
+++ stackqa/src/App.jsx
@@ -0,0 +1,14 @@
+import Routers from "./routes/Routers"
+
+
+
+function App() {
+
+  return (
+    <div>
+      <Routers />
+    </div>
+  )
+}
+
+export default App
---
+++ stackqa/src/routes/Routers.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
+
+function Routers() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
+}
+
+export default Routers;
