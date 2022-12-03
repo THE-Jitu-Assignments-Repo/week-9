eeda33dbda9f341161fade360278723753a1f2b4
mik284 <mikeykamau222@gmail.com>
2022-12-04 00:54:59 +0300

    Added ellpse dropdown for both post and answer

--- stackqa/src/components/cards/QuestionArticle.jsx
+++ stackqa/src/components/cards/QuestionArticle.jsx
@@ -1,70 +1,87 @@
-import React, { useState } from 'react'
-import { AiFillCaretDown, AiFillCaretUp, AiFillSchedule, AiOutlineEllipsis, AiOutlineUser } from 'react-icons/ai';
-import Answers from '../modals/answers/Answers';
+import React, { useState } from "react";
+import {
+  AiFillCaretDown,
+  AiFillCaretUp,
+  AiFillSchedule,
+  AiOutlineCodepen,
+  AiOutlineDelete,
+  AiOutlineEllipsis,
+  AiOutlineSave,
+  AiOutlineUser,
+} from "react-icons/ai";
+import Answers from "../modals/answers/Answers";
 
 function QuestionArticle() {
-      const [isanswer, setIsAnswer] = useState(false);
+  const [isanswer, setIsAnswer] = useState(false);
 
   return (
     <article className="border-b h-auto grid-col-1 grid-flow-row items-center from-sky-200 bg-white p-3">
-            <div className="flex flex-row">
-              <div className="rounded-full bg-blue-400 w-12 h-11 mt-4 flex items-center justify-center">
-                <AiOutlineUser size={20} />
-              </div>
-              <div className="p-2 justify-between flex-grow flex-wrap">
-                <div>Michael Jay</div>
-                <span className="text-gray-300 text-ellipsis">
-                  Asked March 07, 2022
-                </span>
-                <h3 className="leading-relaxed max-w-md hover:text-blue-500">
-                  How can one be good at programming. How can one be good at
-                  programming. How can one be good at programming
-                </h3>
-              </div>
-              <div className="p-2">
-                <AiOutlineEllipsis size={20} />
-              </div>
-            </div>
-            <div className="flex flex-row items-center mb-4">
-              <div className="flex flex-col items-center justify-center pr-2">
-                <AiFillCaretUp
-                  className="hover:text-blue-500 cursor-pointer"
-                  size={20}
-                />
-                <span className="text-xl">32</span>
-                <AiFillCaretDown
-                  className="hover:text-blue-500 cursor-pointer"
-                  size={20}
-                />
-              </div>
-              <div className="p-2 justify-between flex-grow flex-wrap">
-                <h3 className="leading-relaxed max-w-md font-extralight line-clamp-3">
-                  How can one be good at programming. How can one be good at
-                  programming. How can one be good at programming. How can one
-                  be good at programming ... How can one be good at programming.
-                  How can one be good at programming
-                </h3>
-              </div>
-            </div>
-            <div className="flex flex-row items-center rounded-sm justify-between bg-slate-300 m-8  p-2">
-              <div className="items-center justify-between flex flow-row w-full ">
-                <div
-                  className="p-1 flex items-center bg-white rounded-md  hover:text-white hover:bg-blue-300 cursor-pointer"
-                  onClick={() => setIsAnswer((prev) => !prev)}
-                >
-                  <AiFillSchedule className="text-blue-500" size={20} />
-                  <span className="pr-1 pl-1">15 Answers</span>
-                </div>
-                <div className="p-1 flex items-center bg-white rounded-md hover:text-white hover:bg-blue-300 cursor-pointer">
-                  <span className="pr-1 pl-1">Answer</span>
-                </div>
-              </div>
-            </div>
+      <div className="flex flex-row">
+        <div className="rounded-full bg-blue-400 w-12 h-12  mt-4 flex items-center justify-center">
+          <AiOutlineUser size={20} />
+        </div>
+        <div className="p-2 justify-between flex-grow flex-wrap">
+          <div>Michael Jay</div>
+          <span className="text-gray-300 text-ellipsis">
+            Asked March 07, 2022
+          </span>
+          <h3 className="leading-relaxed max-w-md hover:text-blue-500">
+            How can one be good at programming. How can one be good at
+            programming. How can one be good at programming
+          </h3>
+        </div>
+        <div>
+          <div className="peer hover:bg-blue-500 peer cursor-pointer hover:rounded-full hover:text-white">
+            <AiOutlineEllipsis  className="" size={30} />
+          </div>
+            <ul className="h-auto hidden absolute peer-hover:flex hover:flex flex-col  drop-shadow-lg top-46 bg-white shadow-lg font-extralight w-[150px] z-10 rounded-sm">
+                <li className="hover:bg-slate-200 cursor-pointer p-2 flex items-center gap-1 "><AiOutlineDelete />Delete post</li>
+                <li className="hover:bg-slate-200 cursor-pointer p-2 flex items-center gap-1 "><AiOutlineSave />Save post</li>
+                <li className="hover:bg-slate-200 cursor-pointer p-2 flex items-center gap-1 "><AiOutlineCodepen /> Embed post</li>
+            </ul>
 
-            {/* answer modal-content */}
-            {isanswer && <Answers />}
-          </article>
-  )
+        </div>
+      </div>
+      <div className="flex flex-row items-center mb-4">
+        <div className="flex flex-col items-center justify-center pr-2">
+          <AiFillCaretUp
+            className="hover:text-blue-500 cursor-pointer"
+            size={20}
+          />
+          <span className="text-xl">32</span>
+          <AiFillCaretDown
+            className="hover:text-blue-500 cursor-pointer"
+            size={20}
+          />
+        </div>
+        <div className="p-2 justify-between flex-grow flex-wrap">
+          <h3 className="leading-relaxed max-w-md font-extralight line-clamp-3">
+            How can one be good at programming. How can one be good at
+            programming. How can one be good at programming. How can one be good
+            at programming ... How can one be good at programming. How can one
+            be good at programming
+          </h3>
+        </div>
+      </div>
+      <div className="flex flex-row items-center rounded-sm justify-between bg-slate-300 m-8  p-2">
+        <div className="items-center justify-between flex flow-row w-full ">
+          <div
+            className="p-1 flex items-center bg-white rounded-md  hover:text-white hover:bg-blue-300 cursor-pointer"
+            onClick={() => setIsAnswer((prev) => !prev)}
+          >
+            <AiFillSchedule className="text-blue-500" size={20} />
+            <span className="pr-1 pl-1">15 Answers</span>
+          </div>
+          <div className="p-1 flex items-center bg-white rounded-md hover:text-white hover:bg-blue-300 cursor-pointer">
+            <span className="pr-1 pl-1">Answer</span>
+          </div>
+        </div>
+      </div>
+
+      {/* answer modal-content */}
+      {isanswer && <Answers />}
+    </article>
+  );
 }
 
-export default QuestionArticle
+export default QuestionArticle;
--- stackqa/src/components/modals/answers/Answers.jsx
+++ stackqa/src/components/modals/answers/Answers.jsx
@@ -1,57 +1,81 @@
-import React from 'react'
-import { AiFillCaretDown, AiFillCaretUp, AiOutlineEllipsis, AiOutlineSend, AiOutlineUser } from 'react-icons/ai'
-import {GrSend} from 'react-icons/gr'
+import React from "react";
+import {
+  AiFillCaretDown,
+  AiFillCaretUp,
+  AiOutlineCheckCircle,
+  AiOutlineComment,
+  AiOutlineDelete,
+  AiOutlineEllipsis,
+  AiOutlineSend,
+  AiOutlineUser,
+} from "react-icons/ai";
+import { GrSend } from "react-icons/gr";
 
 function Answers() {
   return (
     <div>
-        <div className="border-t h-auto p-5">
-                <div className="flex flex-row  bg-slate-300 p-2 mb-1">
-                  <input
-                    type="text"
-                    className="outline-none font-extralight p-1 pl-2 w-full rounded-l-md caret-slate-400"
-                    placeholder="Write answer ..."
-                  />
-                  <button className='bg-blue-500 rounded-r-md w-20 hover:bg-blue-300 hover:text-white flex-row flex items-center justify-center'><GrSend size={20}/></button>
-                </div>
-                <div className="flex flex-row  bg-slate-300 p-2 rounded-lg">
-                  <div className="flex flex-col justify-start gap-2">
-                    <div className="rounded-full bg-blue-400 w-8 h-8 mt-4 flex items-center justify-center border">
-                      <AiOutlineUser size={15} />
-                    </div>
-                    <div className="flex flex-col items-center justify-center">
-                      <AiFillCaretUp
-                        className="hover:text-blue-500 cursor-pointer" 
-                        size={20}
-                      />
-                      <span className="text-xl">12</span>
-                      <AiFillCaretDown
-                        className="hover:text-blue-500 cursor-pointer"
-                        size={20}
-                      />
-                    </div>
-                  </div>
-                  <div className="p-2 justify-between flex-grow flex-wrap">
-                    <div>John Doe</div>
-                    <span className="text-gray-500 text-ellipsis">
-                      Answered March 08, 2022
-                    </span>
-                    <h3 className="leading-relaxed max-w-md font-semi">
-                      How can one be good at programming. How can one be good at
-                      programming. How can one be good at programming
-                    </h3>
-                  </div>
-                  <div className="p-2">
-                    <AiOutlineEllipsis size={20} />
-                  </div>
-                </div>
-                <div className="pt-1">
-                  <span className="font-extralight">comment</span> |{" "}
-                  <span className="font-extralight">Prefered</span>
-                </div>
-              </div>
+      <div className="border-t h-auto p-5">
+        <div className="flex flex-row  bg-slate-300 p-2 mb-1">
+          <input
+            type="text"
+            className="outline-none font-extralight p-1 pl-2 w-full rounded-l-md caret-slate-400"
+            placeholder="Write answer ..."
+          />
+          <button className="bg-blue-500 rounded-r-md w-20 hover:bg-blue-300 hover:text-white flex-row flex items-center justify-center">
+            <GrSend size={20} />
+          </button>
+        </div>
+        <div className="flex flex-row  bg-slate-300 p-2 rounded-lg">
+          <div className="flex flex-col justify-start gap-2">
+            <div className="rounded-full bg-blue-400 w-8 h-8 mt-4 flex items-center justify-center border">
+              <AiOutlineUser size={15} />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <AiFillCaretUp
+                className="hover:text-blue-500 cursor-pointer"
+                size={20}
+              />
+              <span className="text-xl">12</span>
+              <AiFillCaretDown
+                className="hover:text-blue-500 cursor-pointer"
+                size={20}
+              />
+            </div>
+          </div>
+          <div className="p-2 justify-between flex-grow flex-wrap">
+            <div>John Doe</div>
+            <span className="text-gray-500 text-ellipsis">
+              Answered March 08, 2022
+            </span>
+            <h3 className="leading-relaxed max-w-md font-semi">
+              How can one be good at programming. How can one be good at
+              programming. How can one be good at programming
+            </h3>
+          </div>
+          <div>
+            <div className="peer hover:bg-blue-500 peer cursor-pointer hover:rounded-full hover:text-white">
+              <AiOutlineEllipsis className="" size={30} />
+            </div>
+            <ul className="h-auto hidden absolute peer-hover:flex hover:flex flex-col  drop-shadow-lg top-46 bg-white shadow-lg font-extralight w-[150px] z-10 rounded-sm">
+              <li className="hover:bg-slate-200 cursor-pointer p-2 flex items-center gap-1 ">
+                <AiOutlineComment />comment
+              </li>
+              <li className="hover:bg-slate-200 cursor-pointer p-2 flex items-center gap-1  ">
+                <AiOutlineCheckCircle /> Accept
+              </li>
+              <li className="hover:bg-slate-200 cursor-pointer p-2 flex items-center gap-1 ">
+                <AiOutlineDelete />Delete answer
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="pt-1">
+          <span className="font-extralight"> 3 comments</span> |{" "}
+          <span className="font-extralight text-green-500">Prefered</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Answers
+export default Answers;
