85ebf0b6aead1130e82b24e50e0401fc8f6aa335
mik284 <mikeykamau222@gmail.com>
2022-12-06 20:04:59 +0300

    Created login user routes, controller and struct proc worked

--- questionService/controllers/userControllers/user.js
+++ questionService/controllers/userControllers/user.js
@@ -1,7 +1,8 @@
 const sqlConfig = require('../../config/config')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const {
-    v4:uuidv4
+    v4: uuidv4
 } = require('uuid')
 const mssql = require('mssql')
 const dotenv = require('dotenv')
@@ -30,7 +31,7 @@ module.exports = {
                 password: hash,
                 imageUrl
             }
-            
+
 
             await pool.request()
                 .input("userID", id)
@@ -39,17 +40,57 @@ module.exports = {
                 .input("password", newUser.password)
                 .input("imageUrl", newUser.imageUrl)
                 .execute('sp_registerUser');
-            res.status(200).json({message: 'Reqistered succefully'})
+            res.status(200).json({
+                message: 'Reqistered succefully'
+            })
 
         } catch (error) {
-            res.status(500).json({message: error.message})
+            res.status(500).json({
+                message: error.message
+            })
         }
     },
     loginUser: async (req, res) => {
+        console.log(process.env.SECRET);
         try {
+            const {
+                email,
+                password
+            } = req.body
+            const pool = await mssql.connect(sqlConfig)
+
+            const user = await (await pool.request().input("email", email).execute('sp_loginUser')).recordset[0];
+
+            if (user) {
+                const comparePass = await bcrypt.compare(password, user.password)
+                if (comparePass) {
+                    const {
+                        password,
+                        user_id,
+                        ...rest
+                    } = user;
+                    const token = jwt.sign(rest, process.env.SECRET, {
+                        expiresIn: "10mins"
+                    })
+                    res.status(200).json({
+                        "my Token": token,
+                        message: "login successfully"
+                    })
+                } else {
+                    res.status(401).json({
+                        message: "Password entered is wrong"
+                    })
+                }
+            } else {
+                res.status(402).json({
+                    message: "User not found"
+                })
+            }
 
         } catch (error) {
-
+            res.status(500).json({
+                message: error.message
+            })
         }
     }
 }
