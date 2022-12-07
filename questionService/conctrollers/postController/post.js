70734126553785c4c02e1d609adf59c8c727f8b0
mik284 <mikeykamau222@gmail.com>
2022-12-07 19:05:28 +0300

    Created controllers skeleton structure

---
+++ questionService/conctrollers/postController/post.js
@@ -0,0 +1,87 @@
+const mssql = require('mssql')
+const sqlConfig = require('../../config/config')
+const dotenv = require('dotenv')
+const {v4:uuidv4} = require('uuid')
+
+
+dotenv.config()
+
+module.exports={
+    postQuestion:async()=>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    },
+    getQuestions: async()=>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    },
+    getSingleQuestion:async()=>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    },
+    deleteQuestion:async()=>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    },
+    postAnswer:async()=>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    }, 
+    markPreferred:async()=>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    },
+    vote:async()=>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    }, 
+    commentAnswer:async()=>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    },
+    fetchMyQuestions:async()=>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    },
+    searchQuestions:async()=>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    },
+    questionMostAnswers:async()=>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
