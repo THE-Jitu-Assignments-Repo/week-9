ac5c8fd938bf9ec3225836e2f2c29a446da6a5af
mik284 <mikeykamau222@gmail.com>
2022-12-08 16:42:44 +0300

    Worked on midware to verify token

--- questionService/middleware/verifyToken.js
+++ questionService/middleware/verifyToken.js
@@ -5,14 +5,15 @@ dotenv.config()
 
 
 module.exports={
-    verifyToken: async(res, req, next)=>{
+    verifyToken: async(req, res, next)=>{
             try {
-                const token = req.headers['token']
+                const token = req.headers.authorization
+                console.log(token);
                 if(!token){
                     return res.status(402).json({message: "Not authorized, Please provide a token"})
                 }
 
-                const decodeTokenData= jwt.verify(token, process.env.SECRET)
+                const decodeTokenData= jwt.verify(token.split(" ")[1], process.env.SECRET)
                 req.info=decodeTokenData
             } catch (error) {
                 return res.status(403).json({message: error.message})
--- questionService/routes/questionRoutes.js
+++ questionService/routes/questionRoutes.js
@@ -1,11 +1,12 @@
 const { postQuestion, getQuestions, getSingleQuestion, deleteQuestion } = require('../controllers/postController/post')
+const {verifyToken} = require('../middleware/verifyToken')
 
 const router = require('express').Router()
 
 
 
-router.post('/postquestion', postQuestion)
-router.get('/allquestions', getQuestions)
+router.post('/postquestion', verifyToken, postQuestion)
+router.get('/allquestions', verifyToken,getQuestions)
 router.get('/:id', getSingleQuestion)
 router.get('/delete/:id', deleteQuestion)
 
