2ca03b2d92ef1f840d20b6d8aa8425f9f3aa557f
mik284 <mikeykamau222@gmail.com>
2022-12-09 19:22:06 +0300

    added comment controller

---
+++ questionService/Database/Procedure/answer/getAnswersToQuestion.sql
@@ -0,0 +1,7 @@
+
+CREATE OR ALTER PROC sp_getAnswers(@postID VARCHAR(255))
+As
+BEGIN
+SET NOCOUNT ON;
+	SELECT * FROM Answer_tbl where post_id= @postID;
+END
--- questionService/controllers/commentController/comment.js
+++ questionService/controllers/commentController/comment.js
@@ -11,9 +11,23 @@ dotenv.config()
 module.exports={
      commentAnswer: async (req, res) => {
         try {
-            
-        } catch (error) {
+            const {user_id}= req.info
+            const {answer_id, comment}= req.body
+            const id = uuidv4()
 
+            const pool = await mssql.connect(sqlConfig)
+
+            await pool.request()
+            .input("commentID", id)
+            .input("userID", user_id)
+            .input("answerID", answer_id)
+            .input("comment", comment)
+            .execute('sp_postOrEditComment')
+
+            res.status(200).json({message: "Commented succesfully"})
+
+        } catch (error) {
+            res.status(401).json({message: error.message})
         }
     },
     getComments:async (req, res) => {
