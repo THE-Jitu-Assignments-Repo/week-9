183e86698547b3d01fae101d2cffa3b27adc4587
mik284 <mikeykamau222@gmail.com>
2022-12-16 09:52:52 +0300

    updated mark preferred auth

--- questionService/Database/Procedure/answer/sp_markpreferred.sql
+++ questionService/Database/Procedure/answer/sp_markpreferred.sql
@@ -7,3 +7,16 @@ IF EXISTS(SELECT * FROM Answer_tbl WHERE answer_id=@answerID AND preferred=0)
 ELSE
 	UPDATE Answer_tbl SET preferred=0 WHERE answer_id=@answerID;
 END
+
+CREATE OR ALTER PROC sp_markPreferred(@answerID VARCHAR(255))
+AS
+BEGIN
+SET NOCOUNT ON;
+	DECLARE @exists BIT;
+	SELECT @exists = count(answer_id) FROM Answer_tbl WHERE answer_id=@answerID AND preferred=0;
+	IF @exists > 0
+		UPDATE Answer_tbl SET preferred=1 WHERE answer_id=@answerID;
+ELSE
+	IF @exists > 0
+	UPDATE Answer_tbl SET preferred=0 WHERE answer_id=@answerID;
+END
--- questionService/controllers/answerController/answer.js
+++ questionService/controllers/answerController/answer.js
@@ -7,93 +7,138 @@ const {
 
 dotenv.config()
 
-module.exports={
+module.exports = {
     postAnswer: async (req, res) => {
         try {
-            const {user_id} = req.info
-            const {post_id, answer}=req.body
+            const {
+                user_id
+            } = req.info
+            const {
+                post_id,
+                answer
+            } = req.body
             const id = uuidv4()
 
             const pool = await mssql.connect(sqlConfig)
 
             await pool.request()
-            .input("answerID", id)
-            .input("userID", user_id)
-            .input("postID", post_id)
-            .input("answer", answer)
-            .execute('sp_postOrEditAnswer')
+                .input("answerID", id)
+                .input("userID", user_id)
+                .input("postID", post_id)
+                .input("answer", answer)
+                .execute('sp_postOrEditAnswer')
 
-            res.status(200).json({message: "Question answered succesfully"})
+            res.status(200).json({
+                message: "Question answered succesfully"
+            })
         } catch (error) {
-            res.status(401).json({message: error.message})
+            res.status(401).json({
+                message: error.message
+            })
         }
     },
-    updateAnswer:async (req, res) => {
-         try {
-            const {user_id} = req.info
-            const {post_id, answer}=req.body
-            const id = uuidv4()
-
-            const pool = await mssql.connect(sqlConfig)
-
-            await pool.request()
-            .input("answerID", id)
-            .input("userID", user_id)
-            .input("postID", post_id)
-            .input("answer", answer)
-            .execute('sp_postOrEditAnswer')
-
-            res.status(200).json({message: "Answer updated succesfully"})
-        } catch (error) {
-            res.status(401).json({message: error.message})
-        }
-    },
-    getAnswers:  async (req, res) => {
+    updateAnswer: async (req, res) => {
         try {
-            const {postID} = req.params
+            const {
+                user_id
+            } = req.info
+            const {
+                post_id,
+                answer
+            } = req.body
+            const id = uuidv4()
+
+            const pool = await mssql.connect(sqlConfig)
+
+            await pool.request()
+                .input("answerID", id)
+                .input("userID", user_id)
+                .input("postID", post_id)
+                .input("answer", answer)
+                .execute('sp_postOrEditAnswer')
+
+            res.status(200).json({
+                message: "Answer updated succesfully"
+            })
+        } catch (error) {
+            res.status(401).json({
+                message: error.message
+            })
+        }
+    },
+    getAnswers: async (req, res) => {
+        try {
+            const {
+                postID
+            } = req.params
             const pool = await mssql.connect(sqlConfig)
             const Answers = await (await pool.request().input('postID', postID).execute('sp_getAnswers')).recordset
 
             res.status(200).json(Answers)
 
         } catch (error) {
-            res.status(402).json({message: error.message})
+            res.status(402).json({
+                message: error.message
+            })
         }
     },
     markPreferred: async (req, res) => {
         try {
-            const { id } = req.params
+            const {
+                id
+            } = req.params
             const pool = await mssql.connect(sqlConfig)
 
+            const {
+                user_id
+            } = req.info
+            const recordID = await (await pool.request().input("answerID", id).execute('sp_getTheAnswer')).recordset
+            // console.log(recordID[0].user_id);
+            if (user_id === recordID[0].user_id) {
 
-            await pool.request()
-            .input("answerID", id)
-            .execute('sp_markPreferred')
+                await pool.request()
+                    .input("answerID", id)
+                    .execute('sp_markPreferred')
 
-            res.status(200).json({message: "marked as preferred"})
+                res.status(200).json({
+                    message: "marked as preferred"
+                })
+            }else{
+                res.status(400).json({messgae: "Not authorized"})
+            }
 
         } catch (error) {
-            res.status(401).json({message: error.message})
+            res.status(401).json({
+                message: error.message
+            })
         }
     },
     vote: async (req, res) => {
         try {
-            const {answer_id} = req.body
-            const { user_id }= req.info
+            const {
+                answer_id
+            } = req.body
+            const {
+                user_id
+            } = req.info
 
             const id = uuidv4()
             const pool = await mssql.connect(sqlConfig)
 
             await pool.request()
-            .input("voteID", id)
-            .input("userID", user_id)
-            .input("answerID", answer_id)
-            .execute('sp_upvoteOrDownvote')
+                .input("voteID", id)
+                .input("userID", user_id)
+                .input("answerID", answer_id)
+                .execute('sp_upvoteOrDownvote')
 
-            res.status(200).json({message: "Succefully voted"})
+            res.status(200).json({
+                message: "Succefully voted"
+            })
 
         } catch (error) {
-            res.status(401).json({message: error.message})
+            res.status(401).json({
+                message: error.message
+            })
         }
     }
 }
