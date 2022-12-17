ea5bc7c33e2a6fc1042ef0000a9e13b94d0ba452
mik284 <mikeykamau222@gmail.com>
2022-12-17 19:32:48 +0300

    added comment slice

---
+++ stackqa/src/features/comments/commentSlice.js
@@ -0,0 +1,68 @@
+import {
+    createAsyncThunk,
+    createSlice
+} from "@reduxjs/toolkit"
+import axios from "axios"
+import { toast } from "react-toastify"
+
+
+const initialState = {
+    Comment: [],
+    total: 0
+}
+
+const Token = localStorage.getItem('token')
+
+export const addComment = createAsyncThunk(
+    "comment/addComment",
+    async (commentDetails, {
+        dispatch,
+        rejectWithValue
+    }) => {
+        try {
+            const response = await axios.post('http://localhost:3001/comment', commentDetails, {
+                headers: {
+                    Authorization: `Bearer ${Token}`,
+                }
+            })
+            dispatch(getComment())
+            return response.data
+        } catch (error) {
+            return rejectWithValue(error.message)
+        }
+    }
+)
+export const getComment = createAsyncThunk(
+    "comment/getComment",
+    async (ansID, {
+        rejectWithValue
+    }) => {
+        try {
+            const response = await axios.get('http://localhost:3001/getcomment/', ansID, {
+                headers: {
+                    Authorization: `Bearer ${Token}`,
+                }
+            })
+            return response.data
+
+        } catch (error) {
+            return rejectWithValue(error.message)
+        }
+    }
+)
+export const commentSlice = createSlice({
+        name: 'comment',
+        initialState,
+        extraReducers: (builder) => {
+            builder.addCase(addComment.fulfilled,(state,action)=>{
+                toast.success(action.payload)
+            }),
+            builder.addCase(getComment.fulfilled,(state,action)=>{
+                state.Comment=action.payload
+            })
+        }
+    }
+
+)
+
+export default commentSlice.reducer
--- stackqa/src/store.js
+++ stackqa/src/store.js
@@ -1,12 +1,14 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import answerSlice from './features/answers/answerSlice'
 import userReducer from './features/Auth/UserSlice'
+import commentSlice from './features/comments/commentSlice'
 import questionReducer from './features/questions/QuestionSlice'
 
 const rootReducer = combineReducers({
         user: userReducer,
         questions: questionReducer,
-        answers: answerSlice
+        answers: answerSlice,
+        comments: commentSlice
 })
 
 export const store = configureStore({
