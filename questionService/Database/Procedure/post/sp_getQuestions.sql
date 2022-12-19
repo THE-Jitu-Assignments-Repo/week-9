d8602b6db0712a7ba091ecdab98f1a11d8f26599
mik284 <mikeykamau222@gmail.com>
2022-12-19 10:02:02 +0300

    modified stored proc

--- questionService/Database/Procedure/post/sp_getQuestions.sql
+++ questionService/Database/Procedure/post/sp_getQuestions.sql
@@ -2,5 +2,6 @@ CREATE OR ALTER PROC sp_getQuestions
 AS
 BEGIN
 SET NOCOUNT ON;
-  SELECT * FROM post_tbl WHERE isDeleted = 0;
+  SELECT P.post_id, P.user_id,P.post_date, P.question, P.category, U.username,U.email,U.imageUrl 
+  FROM post_tbl P LEFT JOIN usertbl U ON P.user_id=U.user_id WHERE isDeleted = 0;
 END;
