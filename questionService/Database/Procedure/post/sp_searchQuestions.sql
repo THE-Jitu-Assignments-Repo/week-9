af5353c649b6e5444e56be3e292f8b382b807c21
mik284 <mikeykamau222@gmail.com>
2022-12-19 17:19:06 +0300

    modified search stored proc

--- questionService/Database/Procedure/post/sp_searchQuestions.sql
+++ questionService/Database/Procedure/post/sp_searchQuestions.sql
@@ -1,6 +1,10 @@
+
 CREATE OR ALTER PROC sp_searchQuestions(@text VARCHAR(255))
 AS
 BEGIN
 SET NOCOUNT ON;
-	SELECT * FROM post_tbl WHERE question LIKE '%'+ @text+'%' 
+	SELECT P.post_id, P.user_id,P.post_date, P.question, P.category, U.username,U.email,U.imageUrl,
+  (select count(P.post_id) from post_tbl P where U.user_id= P.user_id and isDeleted=0)totalQst,
+  (select count(A.post_id) from Answer_tbl A where A.post_id=P.post_id)totalAns
+  FROM post_tbl P LEFT JOIN usertbl U ON P.user_id=U.user_id WHERE P.question  LIKE '%'+ @text+'%' OR P.category LIKE '%'+ @text+'%';
 END
