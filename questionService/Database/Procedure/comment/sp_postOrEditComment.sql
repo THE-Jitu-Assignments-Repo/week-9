d93271c13d6373fdb49da5ae4dca4bccf901e04e
mik284 <mikeykamau222@gmail.com>
2022-12-19 13:26:07 +0300

    modified comment sp

--- questionService/Database/Procedure/comment/sp_postOrEditComment.sql
+++ questionService/Database/Procedure/comment/sp_postOrEditComment.sql
@@ -1,4 +1,3 @@
-
 CREATE OR ALTER PROC sp_postOrEditComment(
 	@commentID VARCHAR(255),
 	@userID VARCHAR(255),
@@ -8,8 +7,8 @@ CREATE OR ALTER PROC sp_postOrEditComment(
 AS
 BEGIN
 SET NOCOUNT ON;
-IF EXISTS(SELECT * FROM comment_tbl	WHERE user_id=@userID)
-	UPDATE comment_tbl SET comment_date=GETDATE(), comment=@comment WHERE user_id=@userID;
+IF EXISTS(SELECT * FROM comment_tbl	WHERE comment_id=@commentID)
+	UPDATE comment_tbl SET comment_date=GETDATE(), comment=@comment WHERE comment_id=@commentID;
 ELSE
-	INSERT INTO comment_tbl(comment_id, user_id, answer_id,comment_date,comment) VALUES(@commentID,@userID,@answerID,GETDATE(),@comment)
+	INSERT INTO comment_tbl(comment_id, user_id, answer_id,comment_date,comment) VALUES(@commentID,@userID,@answerID, GETDATE(),@comment)
 END
