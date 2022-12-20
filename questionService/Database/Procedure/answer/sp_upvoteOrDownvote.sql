73cccf924787f274e7ad73dfc6ef6065c39aa045
mik284 <mikeykamau222@gmail.com>
2022-12-20 23:52:24 +0300

    modified vote stored proc

--- questionService/Database/Procedure/answer/sp_upvoteOrDownvote.sql
+++ questionService/Database/Procedure/answer/sp_upvoteOrDownvote.sql
@@ -1,27 +1,37 @@
 CREATE OR ALTER PROC sp_upvoteOrDownvote(
 	@voteID VARCHAR(255),
 	@userID VARCHAR(255),
-	@answerID VARCHAR(255)
+	@answerID VARCHAR(255),
+	@direction VARCHAR(50)
 	)
 AS
 BEGIN
 	SET NOCOUNT ON;
 	DECLARE @exists bit;
 	SELECT @exists = count(vote_id) FROM vote_tbl WHERE user_id=@userID AND answer_id=@answerID AND votes=0
-	IF @exists > 0
-		UPDATE vote_tbl SET votes=1 WHERE user_id=@userID;
-	ELSE
+	IF @exists > 0 and @direction='up'
 	BEGIN
-		SET @exists = 0;
-		SELECT @exists = count(vote_id) FROM vote_tbl WHERE user_id=@userID AND answer_id=@answerID AND votes=1
-		IF @exists > 0
-			UPDATE vote_tbl SET votes=0 WHERE user_id=@userID ;
-		ELSE 
-			INSERT INTO vote_tbl(vote_id, user_id, answer_id, votes) 
-			VALUES(@voteID, @userID, @answerID, 1);	
+		UPDATE vote_tbl SET votes=1 WHERE user_id=@userID AND answer_id=@answerID;
 	END
+			
+	SET @exists = 0;
+	SELECT @exists = count(vote_id) FROM vote_tbl WHERE user_id=@userID AND answer_id=@answerID AND votes=1
+	IF @exists > 0 and @direction = 'down'
+	BEGIN
+		UPDATE vote_tbl SET votes=0 WHERE user_id=@userID AND answer_id=@answerID;
+	END
+			
+	SET @exists = 0;
+	SELECT @exists = count(vote_id) FROM vote_tbl WHERE user_id=@userID AND answer_id=@answerID
+	IF @exists < 1
+	BEGIN
+		INSERT INTO vote_tbl(vote_id, user_id, answer_id, votes) 
+			VALUES(@voteID, @userID, @answerID, 1);
+	END
+			
+	
+	SELECT * FROM vote_tbl WHERE user_id=@userID AND answer_id=@answerID;
 END
-
 
 
 --- TEST--
