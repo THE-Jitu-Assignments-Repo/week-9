9293ce30eeacdb20600c25c0cc8173f21809c251
mik284 <mikeykamau222@gmail.com>
2022-12-21 01:25:55 +0300

    modified mark preffered stored proc

--- questionService/Database/Procedure/answer/sp_markpreferred.sql
+++ questionService/Database/Procedure/answer/sp_markpreferred.sql
@@ -1,12 +1,15 @@
-CREATE OR ALTER PROC sp_markPreferred(@answerID VARCHAR(255))
+CREATE OR ALTER PROC sp_markPreferred(@postID VARCHAR(255),@answerID VARCHAR(255))
 AS
+BEGIN
 BEGIN
 SET NOCOUNT ON;
 	DECLARE @exists BIT;
-	SELECT @exists = count(answer_id) FROM Answer_tbl WHERE answer_id=@answerID AND preferred=0;
+	SELECT @exists = count(A.answer_id) FROM Answer_tbl A WHERE A.answer_id=@answerID and A.post_id=@postID;
 	IF @exists > 0
+		UPDATE Answer_tbl SET preferred=0 where post_id=@postID;
+	END
+	BEGIN
+		IF EXISTS(select * from Answer_tbl where answer_id=@answerID)
 		UPDATE Answer_tbl SET preferred=1 WHERE answer_id=@answerID;
-ELSE
-	IF @exists > 0
-	UPDATE Answer_tbl SET preferred=0 WHERE answer_id=@answerID;
+	END
 END
